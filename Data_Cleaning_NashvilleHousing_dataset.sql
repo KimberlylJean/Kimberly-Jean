cacc5a5114321c9023a89695dbe354c90dec1609
kimy509 <94139712+kimy509@users.noreply.github.com>
2021-11-11 16:23:04 -0500

    Add files via upload

---
+++ Data_Cleaning_NashvilleHousing_dataset.sql
@@ -0,0 +1,159 @@
+---CLEANING DATA IN SQL QUERIES
+
+SELECT *
+FROM PortfolioProjects..NashvilleHousing;
+
+
+--------------------------------------------------------------------------------------------------------------------
+
+--STANDARDIZE DATE FORMAT
+
+SELECT SaleDate,CAST(SaleDate AS date)
+FROM PortfolioProjects..NashvilleHousing;
+
+ALTER TABLE NashvilleHousing
+ADD SaleDateConverted DATE;
+
+UPDATE NashvilleHousing
+SET SaleDateConverted=CONVERT(DATE,SaleDate);
+
+---------------------------------------------------------------------------------------------------------------
+
+--POPULATE PROPERTY ADDRESS DATA
+
+Select *
+FROM PortfolioProjects..NashvilleHousing
+--WHERE PropertyAddress IS NULL;
+ORDER BY ParcelID;
+
+
+Select A.ParcelID,A.PropertyAddress,B.ParcelID,B.PropertyAddress,ISNULL(A.PropertyAddress,B.PropertyAddress)
+FROM PortfolioProjects..NashvilleHousing AS A
+JOIN PortfolioProjects..NashvilleHousing AS B
+ON A.ParcelID=B.ParcelID
+AND A.[UniqueID ]<>B.[UniqueID ]
+WHERE A.PropertyAddress IS NULL;
+
+Update A
+SET PropertyAddress= ISNULL(A.PropertyAddress,B.PropertyAddress)
+FROM PortfolioProjects..NashvilleHousing AS A
+JOIN PortfolioProjects..NashvilleHousing AS B
+ON A.ParcelID=B.ParcelID
+AND A.[UniqueID ]<>B.[UniqueID ]
+WHERE A.PropertyAddress IS NULL;
+
+
+--------------------------------------------------------------------------------------------------------------------
+
+--BREAKING OUT ADDRESS INTO INDIVIDUAL COLUMNS(Address,City,State)
+
+
+Select PropertyAddress
+FROM PortfolioProjects..NashvilleHousing;
+
+SELECT 
+SUBSTRING(PropertyAddress,1,CHARINDEX(',',PropertyAddress)-1) AS Address,
+SUBSTRING(PropertyAddress,CHARINDEX(',',PropertyAddress)+1,LEN(PropertyAddress)) AS State
+FROM PortfolioProjects..NashvilleHousing;
+
+ALTER TABLE NashvilleHousing
+ADD PropertySplitAdress NVARCHAR(255);
+
+UPDATE NashvilleHousing
+SET PropertySplitAdress= SUBSTRING(PropertyAddress,1,CHARINDEX(',',PropertyAddress)-1);
+
+ALTER TABLE NashvilleHousing
+ADD PropertySplitCity NVARCHAR(255);
+
+Update NashvilleHousing
+SET PropertySplitCity=SUBSTRING(PropertyAddress,CHARINDEX(',',PropertyAddress)+1,LEN(PropertyAddress));
+
+---OWNER ADDRESS 
+
+SELECT OwnerAddress
+FROM PortfolioProjects..NashvilleHousing;
+
+SELECT PARSENAME(REPLACE(OwnerAddress,',','.'),1) AS State,
+PARSENAME(REPLACE(OwnerAddress,',','.'),2) AS City,
+PARSENAME(REPLACE(OwnerAddress,',','.'),3) AS Address
+FROM PortfolioProjects..NashvilleHousing;
+
+ALTER TABLE NashvilleHousing
+ADD OwnerSplitAddress NVARCHAR(255);
+
+UPDATE NashvilleHousing
+SET OwnerSplitAddress=PARSENAME(REPLACE(OwnerAddress,',','.'),3);
+
+ALTER TABLE NashvilleHousing
+ADD OwnerSplitCity NVARCHAR(255);
+
+UPDATE NashvilleHousing
+SET OwnerSplitCity=PARSENAME(REPLACE(OwnerAddress,',','.'),2);
+
+ALTER TABLE NashvilleHousing
+ADD OwnerSplitState NVARCHAR(255);
+
+UPDATE NashvilleHousing
+SET OwnerSplitState=PARSENAME(REPLACE(OwnerAddress,',','.'),1);
+
+-----------------------------------------------------------------------------------------------------------------------------
+
+--CHANGE 'Y' AND 'N' TO YES AND NO IN "Sold as Vacant" FIELD
+
+SELECT DISTINCT(SoldAsVacant),COUNT(SoldAsVacant)
+FROM PortfolioProjects..NashvilleHousing
+GROUP BY SoldAsVacant
+ORDER BY 2;
+
+SELECT SoldAsVacant,
+CASE  WHEN SoldASVacant='Y' THEN 'Yes'
+      WHEN SoldAsVacant='N' THEN 'No'
+	  ELSE SoldAsVacant
+	  END
+FROM PortfolioProjects..NashvilleHousing;
+
+UPDATE NashvilleHousing
+SET SoldAsVacant=CASE  WHEN SoldASVacant='Y' THEN 'Yes'
+      WHEN SoldAsVacant='N' THEN 'No'
+	  ELSE SoldAsVacant
+	  END
+FROM PortfolioProjects..NashvilleHousing;;
+
+
+------------------------------------------------------------------------------------------------------------------------
+
+--REMOVE DUPLICATES
+
+
+WITH RowNumCTE AS(
+SELECT*,
+     ROW_NUMBER() OVER(
+	 PARTITION BY ParcelId,
+	              PropertyAddress,
+				  SalePrice,
+				  SaleDate,
+				  LegalReference
+				  ORDER BY 
+				  UniqueID
+				  ) Row_num
+FROM PortfolioProjects..NashvilleHousing
+)
+SELECT*
+FROM RowNumCTE
+WHERE Row_num>1
+ORDER BY PropertyAddress;
+
+-----------------------------------------------------------------------------------------------------------------------
+
+---DELETE UNUSED COLUMNS
+
+SELECT *
+FROM PortfolioProjects..NashvilleHousing;
+
+ALTER TABLE NashvilleHousing
+DROP COLUMN OwnerAddress,TaxDistrict,PropertyAddress;
+
+ALTER TABLE NashvilleHousing
+DROP COLUMN SaleDate;
+
+-----------------------------------------------------------------------------------------------------------------------------
---
+++ Data_Exploration_COVID-19_dataset.sql
@@ -0,0 +1,197 @@
+--COVID 19 DATA EXPLORATION
+--USING:JOINS,CTE'S,TEMPORARY TABLES,WINDOWS FUNCTIONS,AGGREGATE FUNCTIONS,VIEWS,DATA TYPE CONVERSION
+
+
+
+
+SELECT *
+FROM PortfolioProjects..CovidDeaths
+WHERE continent IS NOT NULL
+ORDER BY 3,4;
+
+
+--SELECT THE DATA WE ARE USING
+
+SELECT Location,Date,Total_cases,new_cases,total_deaths,population
+FROM PortfolioProjects..CovidDeaths
+ORDER BY 1,2;
+
+--Looking at the total cases vs total deaths
+--Shows the likelihood of dying if you contract covid in Haiti
+
+
+SELECT Location,Date,Total_cases,total_deaths,(total_deaths/total_cases)*100 AS DeathPercentage
+FROM PortfolioProjects..CovidDeaths
+WHERE location='Haiti';
+ORDER BY 1,2;
+
+--Looking at the total cases vs Population
+--Shows what percentage of the poulation got covid
+
+SELECT Location,Date,population,Total_cases,(total_cases/population)*100 AS InfectionRate
+FROM PortfolioProjects..CovidDeaths
+WHERE location ='Haiti'
+ORDER BY 1,2;
+
+--The countries with highest infection rate compared to population
+
+SELECT Location,population,MAX(Total_cases) AS HighestInfectionCount,MAX((total_cases/population))*100 AS PercentPopulationInfected
+FROM PortfolioProjects..CovidDeaths
+GROUP BY Location,population
+ORDER BY PercentPopulationInfected DESC;
+
+--Showing Countries with highest Death Count per Population
+
+SELECT Location,MAX(CAST(Total_deaths as int)) AS TotalDeathCount
+FROM PortfolioProjects..CovidDeaths
+WHERE continent IS NOT NULL
+GROUP BY Location
+ORDER BY TotalDeathCount DESC;
+
+--BREAKING DATA EXPLORATION BY CONTINENT
+
+--Showing Continents with highest Death Count per Population
+
+SELECT continent,MAX(CAST(Total_deaths as int)) AS TotalDeathCount
+FROM PortfolioProjects..CovidDeaths
+WHERE continent IS NOT NULL
+GROUP BY continent
+ORDER BY TotalDeathCount DESC;
+
+CREATE VIEW TotalDeathCountPerContinent AS
+SELECT continent,MAX(CAST(Total_deaths as int)) AS TotalDeathCount
+FROM PortfolioProjects..CovidDeaths
+--WHERE location ='Haiti'
+WHERE continent IS NOT NULL
+GROUP BY continent
+--ORDER BY TotalDeathCount DESC;
+
+
+
+
+--GLOBAL NUMBERS
+
+SELECT SUM(new_cases) AS Total_cases,SUM(cast (new_deaths AS INT)) AS Total_deaths,SUM(CAST(new_deaths AS INT))/SUM(new_cases)*100 AS DeathPercentage
+FROM PortfolioProjects..CovidDeaths
+WHERE continent IS NOT NULL
+ORDER BY 1,2;
+
+SELECT Date,SUM(new_cases) AS Total_cases,SUM(cast (new_deaths AS INT)) AS Total_deaths,SUM(CAST(new_deaths AS INT))/SUM(new_cases)*100 AS DeathPercentage
+FROM PortfolioProjects..CovidDeaths
+WHERE continent IS NOT NULL
+GROUP BY date
+ORDER BY 1,2;
+
+
+--COVID VACCINATIONS
+SELECT*
+FROM PortfolioProjects..CovidDeaths AS Dea
+JOIN PortfolioProjects..CovidVaccinations AS Vac
+ON Dea.location=Vac.location
+AND Dea.date=Vac.date;
+
+--Looking at Total Population vs Vaccinations
+--Shows the percentage of the population that has recieved at least one Covid Vaccine
+
+SELECT dea.continent,dea.location,dea.date,dea.population,vac.new_vaccinations
+,SUM(CAST(vac.new_vaccinations AS BIGINT)) OVER (Partition by dea.Location ORDER BY dea.location,dea.date) AS RollingPeopleVaccinated
+FROM  PortfolioProjects..CovidDeaths AS Dea
+JOIN PortfolioProjects..CovidVaccinations AS Vac
+    ON dea.location=vac.location
+    AND dea.date=vac.date
+WHERE dea.continent IS NOT NULL
+ORDER BY 2,3;
+
+--Using a Common Table Expression to perform Calculation on Partition By in previous query
+
+WITH POPvsVAC(Continent,location,date,population,new_vaccinations,RollingPeopleVaccinated)
+AS
+(SELECT dea.continent,dea.location,dea.date,dea.population,vac.new_vaccinations
+,SUM(CAST(vac.new_vaccinations AS BIGINT)) OVER (Partition by dea.Location ORDER BY dea.location,dea.date) AS RollingPeopleVaccinated
+FROM  PortfolioProjects..CovidDeaths AS Dea
+JOIN PortfolioProjects..CovidVaccinations AS Vac
+    ON dea.location=vac.location
+    AND dea.date=vac.date
+WHERE dea.continent IS NOT NULL
+--ORDER BY 2,3);
+)
+SELECT*,(RollingPeopleVaccinated/population)*100
+FROM POPvsVAC;
+
+
+
+--Using a temporary table to perform Calculation on Partition By in previous query
+
+DROP TABLE IF EXISTS #PercentageOfPopulationvaccinated
+CREATE TABLE #PercentageOfPopulationvaccinated
+(
+Continent NVARCHAR(255),
+location NVARCHAR(255),
+date DATETIME,
+population NUMERIC,
+New_vaccinations NUMERIC,
+RollingPeopleVaccinated NUMERIC
+)
+
+INSERT INTO #PercentageOfPopulationvaccinated
+SELECT dea.continent,dea.location,dea.date,dea.population,vac.new_vaccinations
+,SUM(CAST(vac.new_vaccinations AS BIGINT)) OVER (Partition by dea.Location ORDER BY dea.location,dea.date) AS RollingPeopleVaccinated
+FROM  PortfolioProjects..CovidDeaths AS Dea
+JOIN PortfolioProjects..CovidVaccinations AS Vac
+    ON dea.location=vac.location
+    AND dea.date=vac.date
+
+SELECT*,(RollingPeopleVaccinated/population)*100
+FROM #PercentageOfPopulationvaccinated;
+
+
+--Creating View to store data later for visualization
+
+
+CREATE VIEW PercentagePopulationvaccinated AS
+SELECT dea.continent,dea.location,dea.date,dea.population,vac.new_vaccinations
+,SUM(CAST(vac.new_vaccinations AS BIGINT)) OVER (Partition by dea.Location ORDER BY dea.location,dea.date) AS RollingPeopleVaccinated
+FROM  PortfolioProjects..CovidDeaths AS Dea
+JOIN PortfolioProjects..CovidVaccinations AS Vac
+    ON dea.location=vac.location
+    AND dea.date=vac.date
+where dea.continent IS NOT NULL
+--ORDER BY 2,3
+
+SELECT *
+FROM PercentagePopulationvaccinated;
+
+
+
+--QUERIES USED FOR TABLEAU PROJECT
+
+*/
+
+---1. 
+SELECT SUM(new_cases) AS total_cases,SUM(CAST(new_deaths AS INT)) AS total_deaths, SUM(CAST(new_deaths AS INT))/SUM(new_cases)*100 AS DeathPercentage
+FROM PortfolioProjects..CovidDeaths
+WHERE continent IS NOT NULL
+ORDER BY 1,2;
+
+---2.
+
+SELECT location, SUM(CAST(new_deaths AS INT)) AS TotalDeathCount
+FROM PortfolioProjects..CovidDeaths
+WHERE continent IS NOT NULL
+AND location NOT IN ('World','European Union','International')
+GROUP BY location
+ORDER BY TotalDeathCount DESC;
+
+---3.
+
+SELECT location,Population,MAX(total_cases) AS HighestInfectionCount, MAX((total_cases/population))*100 AS PercentPopulationInfected
+FROM PortfolioProjects..CovidDeaths
+GROUP BY location,population
+ORDER BY PercentPopulationInfected DESC;
+
+---4.
+
+SELECT location,Population,date,MAX(total_cases) AS HighestInfectionCount,MAX((total_cases/population))*100 AS PercentPopulationInfected
+FROM PortfolioProjects..CovidDeaths
+GROUP BY location,population,date
+ORDER BY PercentPopulationInfected DESC;
