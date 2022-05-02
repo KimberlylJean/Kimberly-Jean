b2473611a80b8e5ef33db62487289344d76755c3
Kimberly Jean <94139712+KimberlylJean@users.noreply.github.com>
2022-05-02 12:32:24 -0400

    Update Sales dataset Exploration.sql

--- Sales dataset Exploration.sql
+++ Sales dataset Exploration.sql
@@ -104,7 +104,7 @@ from rfm_calc as c
 select CUSTOMERNAME , rfm_recency, rfm_frequency, rfm_monetary,
 	case 
 		when rfm_cell_string in (111, 112 , 121, 122, 123, 132, 211, 212, 114, 141) then 'lost_customers'  --lost customers
-		when rfm_cell_string in (133, 134, 143, 244, 334, 343, 344, 144) then 'slipping away, cannot lose' -- (Big spenders who haven’t purchased lately) slipping away
+		when rfm_cell_string in (133, 134, 143, 244, 334, 343, 344, 144) then 'slipping away, cannot lose' -- (Big spenders who havenâ€™t purchased lately) slipping away
 		when rfm_cell_string in (311, 411, 331) then 'new customers'
 		when rfm_cell_string in (222, 223, 233, 322) then 'potential churners'
 		when rfm_cell_string in (323, 333,321, 422, 332, 432) then 'active' --(Customers who buy often & recently, but at low price points)
@@ -148,3 +148,8 @@ from PortfolioDB.dbo.sales_data_sample
 where country = 'USA'
 group by COUNTRY,YEAR_ID, PRODUCTLINE
 order by 4 desc;
+
+
+Tableau Links: 
+https://public.tableau.com/app/profile/kimberly6871/viz/SalesDashboard-1_16500729651740/SalesDashboard
+https://public.tableau.com/app/profile/kimberly6871/viz/SalesDashboard-2_16500732420150/SalesDashboard2
